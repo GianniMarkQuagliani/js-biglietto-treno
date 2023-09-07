4c5564cb8568b49a62cc69d442949d0a4d6fe317
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-09-07 15:05:33 +0200

    [add] pseudo codice

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//Pseudo codice
+/* 
+Inizio del programma
+
+1. Dichiaro tutte le mie variabili.
+2. Chiedi all'utente di inserire il proprio nome.
+3. Chiedi all'utente di inserire il numero di chilometri da percorrere.
+4. Chiedi all'utente di inserire la propria età.
+5. Calcoliamo il prezzo del biglietto.
+6. Vediamo se l'età dell'utente è inferiore o uguale a 18 anni.
+7. Applichiamo uno sconto del 20% al prezzo base.
+8. Altrimenti, se l'età dell'utente è maggiore o uguale a 65 anni.
+9. Applichiamo uno sconto del 40% al prezzo base
+10. Altrimenti facciamo prezzo finale uguale al prezzo base.
+11. Dobbiamo formattere il prezzo finale con massimo di due decimali.
+12. Dobbiamo visualizzare un messaggio con il nime dell'utente, il numero di chilomentri, l'età e il prezzo del biglietto
+
+Fine programma
+*/
