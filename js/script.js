4ae8a2f233e8496d761807186c2ae7de54082f9a
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-09-07 15:34:28 +0200

    [add] Calc ticket price based on distance in km

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
 //Pseudo codice
 /* 
 Inizio del programma
@@ -17,18 +18,39 @@ Inizio del programma
 
 Fine programma
 */
+/**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
 
+
+
+/**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
 //1. Dichiaro tutte le mie variabili
 let name, km, age, price, finalPrice
+
 //2. Chiedi all'utente di inserire il proprio nome.
 name = prompt('Inserisci il tuo nome:');
-//3. Chiedi all'utente di inserire il numero di chilometri da percorrere.
-km = prompt('Inserisci I km che vuoi percorrere:');
 
+//3. Chiedi all'utente di inserire il numero di chilometri da percorrere.
+km = parseInt(prompt('Inserisci I km che vuoi percorrere:'));
+
+//4. Chiedi all'utente di inserire la propria età.
+age = parseInt(prompt('Inserisci la tua età:'));
+
+//Calcola il prezzo del biglietto basato sulla distanza in chilometri.
+price = km * 0.21;
+
+
+
+
+
+
+
+/**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
 //Prendo l'elemento dove stamperò name
 document.getElementById('name').innerHTML = name;
 //Prendo l'elemento dove stamperò km
 document.getElementById('km').innerHTML = km;
+
+/**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
 //console.log
 console.log('Nome:', name);
 console.log('Km:', km);
